3f69ff5b94f863b160a68d15b634c9745d1fefe6
OpswhizMir <jalees01hassan@gmail.com>
2026-05-30 23:46:36 +0530

    updating agent.py

--- agent.py
+++ agent.py
@@ -7,7 +7,7 @@ from openai import OpenAI
 #client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 client = OpenAI(
     base_url="https://openrouter.ai/api/v1",
-    api_key=os.getenv("OPENROUTER_API_KEY")    
+    api_key=os.environ.get("OPENROUTER_API_KEY")    
 
 )
 def run_coral_query(sql_query):
